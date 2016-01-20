eed206bae69c923f56bf5c7138220b46423965f0
genagain <ohta.g@husky.neu.edu>
2016-01-19 20:55:31 -0500

    getting events for boston python

--- meetup_events.py
+++ meetup_events.py
@@ -1,6 +1,12 @@
 import requests
+import ipdb
 
 f = open('api_key.txt','r')
 api_key = f.read()
 api_key = api_key.rstrip()
-print(api_key)
+
+params = {"sign":"true", "key":api_key, "group_urlname":"bostonpython"}
+request = requests.get("http://api.meetup.com/2/events", params)
+response = request.json()
+events = response['results']
+
