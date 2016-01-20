9e501eaf46edfe79006b24e765f0a34ac24e5af9
genagain <ohta.g@husky.neu.edu>
2016-01-20 08:05:17 -0500

    formatted the date correctly

--- meetup_events.py
+++ meetup_events.py
@@ -1,10 +1,13 @@
 import requests
-import datetime
+from datetime import datetime, timedelta
 import ipdb
 
 f = open('api_key.txt','r')
 api_key = f.read()
 api_key = api_key.rstrip()
+present = datetime.now()
+start = present - timedelta(days=present.weekday())
+end = start + timedelta(days=4)
 
 meetups = ['bostonpython',
            'Maptime-Boston', 
@@ -29,14 +32,23 @@ params = {"sign":"true", "key":api_key, "group_urlname":"bostonpython"}
 request = requests.get("http://api.meetup.com/2/events", params)
 response = request.json()
 events = response['results']
-datetime = datetime.datetime.fromtimestamp(events[0]['time'] / 1e3)
-group = events[0]['group']['name']
-name = events[0]['name']
-url = events[0]['event_url']
-duration = events[0]['duration']
-# ipdb.set_trace()
+datetime = datetime.fromtimestamp(events[0]['time'] / 1e3)
+meetups = []
+if start < datetime and datetime < end:
+  group = events[0]['group']['name']
+  name = events[0]['name']
+  url = events[0]['event_url']
+  duration = events[0]['duration']
+  meetup = { 'name':name,
+             'group':group,
+             'datetime':datetime.strftime("%A, %B %-d"),
+             'start_time':0,
+             'finish_time':1,
+             'url':url }
+  ipdb.set_trace()
+  meetups.append(meetup)
 
 ## TODO: Get events from all meetups
 ## TODO: Filter the meetups down to ones happening this week
-## Write the name, group, time, duration, and url to CSV
+## Write the name, group, date, start time, finish time, and url to CSV
 
