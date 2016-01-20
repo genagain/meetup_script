f6f1cc83f381d92f3ca7265e12b23e125d34090c
genagain <ohta.g@husky.neu.edu>
2016-01-19 21:34:52 -0500

    adding meetups

--- meetup_events.py
+++ meetup_events.py
@@ -5,7 +5,25 @@ f = open('api_key.txt','r')
 api_key = f.read()
 api_key = api_key.rstrip()
 
-meetups = ['bostonpython']
+meetups = ['bostonpython',
+           'Maptime-Boston', 
+           'ACM-Boston',
+           'Cambridge-Hackspace',
+           'Boston-Data-Mining',
+           'SocialDataBoston',
+           'Boston-Digital-Analytics-Meetup',
+           'BigDataBoston',
+           'Boston-Algorithmic-Trading',
+           'The-Data-Scientist',
+           'Big-Data-Developers-in-Boston',
+           'PyData-Boston',
+           'BigDBN',
+           'graphdb-boston',
+           'FREE-Big-Data-Hands-On-Workshops',
+           'julia-cajun',
+           'The-Boston-Cassandra-Users',
+           'Happy-Data-Hour-Boston'
+           ]
 params = {"sign":"true", "key":api_key, "group_urlname":"bostonpython"}
 request = requests.get("http://api.meetup.com/2/events", params)
 response = request.json()
