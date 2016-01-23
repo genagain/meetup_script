1f169a634773958185bcf852c620b8115158a528
genagain <ohta.g@husky.neu.edu>
2016-01-22 19:28:08 -0500

    only writing events that are happening in the current week

--- meetup_events.py
+++ meetup_events.py
@@ -1,13 +1,15 @@
 import requests
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import os
 import csv
 import ipdb
 
+#opening, reading and cleaning api key
 f = open('api_key.txt','r')
 api_key = f.read()
 api_key = api_key.rstrip()
 
+#deducing Monday and Friday of this week
 present = datetime.now()
 start = present - timedelta(days=present.weekday())
 end = start + timedelta(days=4)
@@ -34,34 +36,38 @@ all_meetups = ['bostonpython',
            'Happy-Data-Hour-Boston']
 meetups = []
 
+#Getting all of the meetup events
 for meetup in all_meetups:
   params = {"sign":"true", "key":api_key, "group_urlname":meetup}
   request = requests.get("http://api.meetup.com/2/events", params)
   response = request.json()
   events = response['results']
+
   for event in events:
     datetime = datetime.fromtimestamp(event['time'] / 1e3)
-    # if start < datetime and datetime < end:
-    group = str(event['group']['name'])
-    name = str(event['name'])
-    url = str(event['event_url'])
-    start_time = datetime.strftime("%-I:%M")
-    meetup = { 'name':name,
-               'group':group,
-               'datetime':datetime.strftime("%A, %B %-d"),
-               'start_time':start_time,
-               'url':url }
-    meetups.append(meetup)
+
+    #Checking to see if the event is happening this week
+    if start < datetime and datetime < end:
+
+      #Parsing the event data
+      group = str(event['group']['name'])
+      name = str(event['name'])
+      url = str(event['event_url'])
+      start_time = datetime.strftime("%-I:%M")
+      meetup = { 'name':name,
+                 'group':group,
+                 'datetime':datetime.strftime("%A, %B %-d"),
+                 'start_time':start_time,
+                 'url':url }
+      meetups.append(meetup)
 
 os.remove('meetups.csv')
 
+#Writing all the meetups happening this week to a CSV
 with open('meetups.csv', 'w') as csvfile:
   fieldnames = ['name', 'url', 'group', 'datetime', 'start_time']
   writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
   writer.writeheader()
   for meetup in meetups:
     writer.writerow(meetup)
-## TODO: Get events from all meetups
-## TODO: Filter the meetups down to ones happening this week
-## Write the name, group, date, start time, finish time, and url to CSV
 
